02e13d9b0bdd12c833b8c18a6993bb1aaad04943
b3m1ch4 <b3.m1ch4@gmail.com>
2018-10-08 15:32:33 -0400

    Connect to apiUrl
    Connect

--- src/apiConfig.js
+++ src/apiConfig.js
@@ -1,6 +1,6 @@
 let apiUrl
 const apiUrls = {
-  production: 'https://tweeter-remote-0808.herokuapp.com/',
+  production: 'https://tweeter-remote-0808.herokuapp.com',
   development: 'http://localhost:4741'
 }
 
